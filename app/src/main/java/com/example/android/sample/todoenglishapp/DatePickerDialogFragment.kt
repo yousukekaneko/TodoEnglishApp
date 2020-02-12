4108cb6cd1917a82fdafdef9e350a11f6de70a0c
yousukekaneko <kaneko1987-0402@i.softbank.jp>
2020-02-12 09:07:05 +0900

    DatePickerDialogFragmentにリスナーを設定

--- app/src/main/java/com/example/android/sample/todoenglishapp/DatePickerDialogFragment.kt
+++ app/src/main/java/com/example/android/sample/todoenglishapp/DatePickerDialogFragment.kt
@@ -2,12 +2,33 @@ package com.example.android.sample.todoenglishapp
 
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
 class DatePickerDialogFragment: DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    var listener: OnDateSetListner? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnDateSetListner) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnDateSetListner {
+        fun onDateSelected()
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val calendar = Calendar.getInstance()
